6b849e16b620a36e0c735c216de03b3aef15f80e
andrey-satyukov <1846120@gmail.com>
2023-06-28 22:27:05 +0500

    первый

---
+++ src/main/java/com/example/demo/StringListImpl.java
@@ -0,0 +1,84 @@
+package com.example.demo;
+
+public class StringListImpl implements StringList{
+    private static final int DEFAULT_INITIAL_CAPACITY = 20;
+    private final String[] data;
+    private int size;
+
+    public StringListImpl() {
+        this.data = new String[DEFAULT_INITIAL_CAPACITY];
+    }
+    public StringListImpl(int initialCapacity) {
+        this.data = new String[initialCapacity];
+        this.size = 0;
+    }
+    @Override
+    public String add(String item) {
+        return null;
+    }
+
+    @Override
+    public String add(int index, String item) {
+        return null;
+    }
+
+    @Override
+    public String set(int index, String item) {
+        return null;
+    }
+
+    @Override
+    public String remove(String item) {
+        return null;
+    }
+
+    @Override
+    public String remove(int index) {
+        return null;
+    }
+
+    @Override
+    public boolean contains(String item) {
+        return false;
+    }
+
+    @Override
+    public int indexOf(String item) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(String item) {
+        return 0;
+    }
+
+    @Override
+    public String get(int index) {
+        return null;
+    }
+
+    @Override
+    public boolean equals(StringList otherList) {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public String[] toArray() {
+        return new String[0];
+    }
+}
