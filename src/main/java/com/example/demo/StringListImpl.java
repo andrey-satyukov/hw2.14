f039b785bfcd4da487c709925baab310d85c9a82
andrey-satyukov <1846120@gmail.com>
2023-07-05 13:23:51 +0500

    первый

--- src/main/java/com/example/demo/StringListImpl.java
+++ src/main/java/com/example/demo/StringListImpl.java
@@ -1,84 +1,162 @@
 package com.example.demo;
 
-public class StringListImpl implements StringList{
-    private static final int DEFAULT_INITIAL_CAPACITY = 20;
+import java.util.Arrays;
+import java.util.Objects;
+
+public class StringListImpl implements StringList {
     private final String[] data;
     private int size;
 
     public StringListImpl() {
-        this.data = new String[DEFAULT_INITIAL_CAPACITY];
+        this.data = new String[10];
     }
-    public StringListImpl(int initialCapacity) {
-        this.data = new String[initialCapacity];
-        this.size = 0;
+
+    public StringListImpl(int initSize) {
+        this.data = new String[initSize];
     }
+
     @Override
     public String add(String item) {
-        return null;
+        checkSize();
+        checkItem(item);
+        data[size++] = item;
+        return item;
     }
 
     @Override
     public String add(int index, String item) {
-        return null;
+        checkSize();
+        checkItem(item);
+        checkIndex(index);
+
+        if (index == size) {
+            data[size++] = item;
+            return item;
+        }
+
+        System.arraycopy(data, index, data, index + 1, size - index);
+        size++;
+        data[index] = item;
+
+        return item;
     }
 
     @Override
     public String set(int index, String item) {
-        return null;
+        checkIndex(index);
+        checkItem(item);
+        data[index] = item;
+        return item;
     }
 
     @Override
     public String remove(String item) {
-        return null;
+        checkItem(item);
+
+        int index = indexOf(item);
+        return remove(index);
     }
 
     @Override
     public String remove(int index) {
-        return null;
+        checkIndex(index);
+
+        String item = data[index];
+
+        if (index == size) {
+            data[size--] = null;
+            return item;
+        }
+        System.arraycopy(data, index + 1, data, index, size - index );
+
+        size--;
+        return item;
     }
 
     @Override
     public boolean contains(String item) {
-        return false;
+        return indexOf(item) != -1
+                ;
     }
 
     @Override
     public int indexOf(String item) {
-        return 0;
+        for (int i = 0; i < size; i++) {
+            String s = data[i];
+            if (s.equals(item)) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     @Override
     public int lastIndexOf(String item) {
-        return 0;
+        for (int i = size - 1; i >= 0; i--) {
+            String s = data[i];
+            if (s.equals(item)) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     @Override
     public String get(int index) {
-        return null;
+        checkIndex(index);
+
+        return data[index];
     }
 
     @Override
     public boolean equals(StringList otherList) {
-        return false;
+        if (size != otherList.size()) {
+            return false
+            ;
+        }
+        for (int i = 0; i < size; i++) {
+            if (!Objects.equals(this.get(i), otherList.get(i))) {
+                return false;
+            }
+        }
+        return true;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size();
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     @Override
     public void clear() {
-
+        size = 0;
     }
 
     @Override
     public String[] toArray() {
-        return new String[0];
+        return Arrays.copyOf(data, size);
+    }
+
+    private void checkItem(String item) {
+        if (item == null) {
+            throw new IllegalArgumentException("Item не может быть null");
+        }
+    }
+
+    private void checkSize() {
+        if (size == data.length) {
+            throw new IllegalArgumentException("Массив полный");
+        }
+    }
+
+    private void checkIndex(int index) {
+        if (index < 0 || index > size) {
+            throw new IllegalArgumentException()
+        }
     }
 }
